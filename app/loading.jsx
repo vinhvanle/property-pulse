60cdd6b6c21cefd0f24abcfcad5ef2a74cd68c3f
Vinh Van Le <56675876+vinhvanle@users.noreply.github.com>
2024-03-21 11:22:11 +0700

    Add custom 404 and loading pages

---
+++ app/loading.jsx
@@ -0,0 +1,18 @@
+'use client';
+import ClipLoader from 'react-spinners/ClipLoader.js';
+const LoadingPage = ({ loading }) => {
+  const override = {
+    display: 'block',
+    margin: '100px auto',
+  };
+  return (
+    <ClipLoader
+      color='#3b82f6'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label='Loading Spinner'
+    />
+  );
+};
+export default LoadingPage;
