b4ce02f4c2f36c7926627e2134ee84b6124bd8fc
Vinh Van Le <56675876+vinhvanle@users.noreply.github.com>
2024-03-21 16:30:11 +0700

    Fixed the code to use the database instead

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,12 +1,14 @@
 import connectDB from '@/config/database.js';
+import Property from '@/models/Property.js';
 
+// GET /api/properties
 export const GET = async (request) => {
   try {
     await connectDB();
 
-    return new Response(JSON.stringify({ message: 'Hello World' }), {
-      status: 200,
-    });
+    const properties = await Property.find({});
+
+    return Response.json(properties);
   } catch (error) {
     console.log(error);
     return new Response('Something Went Wrong', { status: 500 });
